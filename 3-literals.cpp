605cc70070b251b80ac8ba8c53a77c576c414421
Zakhar-Khmurych <68910550+Zakhar-Khmurych@users.noreply.github.com>
2021-11-24 12:01:41 +0200

    Create 3-literals.cpp

---
+++ 3-literals.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <map>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Product {
+  std::string name;
+  int price;
+};
+
+void printProduct(Product item) {
+  printf("%s: %d\n", item.name.c_str(), item.price);
+}
+
+void printProducts(std::vector<Product> items) {
+  for (int i = 0; i < items.size(); i++) {
+    printProduct(items[i]);
+  }
+}
+
+int main() {
+  std::map<std::string, std::vector<Product>> purchase {
+    { "Electronics", {
+      { "Laptop", 1500 },
+      { "Keyboard", 100 },
+      { "HDMI cable", 10 },
+    } },
+    { "Textile", {
+      { "Bag", 50 },
+    } },
+  };
+
+  vector electronics = purchase["Electronics"];
+  printf("Electronics:\n");
+  printProducts(electronics);
+
+  vector textile = purchase["Textile"];
+  printf("\nTextile:\n");
+  printProducts(textile);
+
+  Product bag = textile[0];
+  printf("\nSingle element:\n");
+  printProduct(bag);
+
+  int price = purchase["Electronics"][2].price;
+  printf("\nHDMI cable price is %d\n", price);
+}
