1ded9975cf396f5e4edc0ddb469a956a37849a12
Zakhar-Khmurych <68910550+Zakhar-Khmurych@users.noreply.github.com>
2021-12-01 11:52:04 +0200

    Update 1-array.py

--- 1-array.py
+++ 1-array.py
@@ -1,8 +1,2 @@
 ages = sorted([10, 8, 12, 15, 15, 17, 18, 18, 13, 23, 19, 20])
-
-first = ages[0]
-last = ages[-1]
-
-dictionary = { 'first': first, 'last': last }
-
-print(dictionary)
+print(ages[0], ages[-1])
