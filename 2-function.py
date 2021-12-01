555a2fd806b05456dfb39f0990c59466cb14daf2
Zakhar-Khmurych <68910550+Zakhar-Khmurych@users.noreply.github.com>
2021-12-01 11:12:09 +0200

    Update 2-function.py

--- 2-function.py
+++ 2-function.py
@@ -4,5 +4,5 @@ def getFirstAndLast(array):
   return first, last
 
 ages = sorted([10, 22, 34, 11, 12, 12, 15, 15, 17, 18, 18, 19, 20])
-first, last = getFirstAndLast(ages)
-print({ 'first': first, 'last': last })
+
+print(getFirstAndLast(ages))
